1531a691afb161fc15861adbf9064fd5577ff5ae
GabrielMassolari <gabrielmassolari@hotmail.com>
2022-12-02 16:51:44 -0300

    Initial commit

---
+++ src/br/edu/ifes/si/trabtpa/Main.java
@@ -0,0 +1,16 @@
+package br.edu.ifes.si.trabtpa;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
